18272392bc118c3e6d667a7e40a5a0150f21c61b
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-08 00:14:57 -0400

    Add files via upload

--- Ch_1/linear_alg1.py
+++ Ch_1/linear_alg1.py
@@ -17,5 +17,26 @@ Matrix:
 [0 1 3 | 3]
 [1 2 2 | -5]
 [5 9 7 | 2]
+
+Interchange rows 1 & 2:
+[1 2 2 | -5]
+[0 1 3 | 3]
+[5 9 7 | 2]
+
+Row operation to change entry in 3rd row, column 1 to a 0:
+(subtract 5 * row 1)
+[1 2 2 | -5]
+[0 1 3 | 3]
+[0 -1 -3 | -23]
+
+Add row 2 to row 3 to zero out the left side of row 3:
+[1 2 2 | -5]
+[0 1 3 | 3]
+[0 0 0 | -20]
+
+This problem has no solution because the 3rd row is the inequality:
+0 = 20
 '''
+
+
 
