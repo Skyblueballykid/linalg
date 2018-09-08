730fe752032e38644b51969b57d8a172aa6bba5c
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-07 23:59:12 -0400

    Add files via upload

---
+++ linear_alg1.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy import linalg as lg
+from numpy.linalg import solve
+from numpy.linalg import eig
+from scipy.integrate import quad
+
+# MAT 350 - Linear Algebra
+# Homework 1
+
+# Question 1 
+'''
+x^2 + 3x^3 = 3
+x^1 + 2x^2 + 2x^3 = -5
+5x^1 + 9x^2 + 7x^3 = 2
+
+Matrix:
+[0 1 3 | 3]
+[1 2 2 | -5]
+[5 9 7 | 2]
+'''
+
