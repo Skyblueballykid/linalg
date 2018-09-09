d51fcab759b71158f7b7f6ca93a316f2872e0449
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-08 23:23:42 -0400

    Questions 1-6

--- Ch_1/linear_alg1.py
+++ Ch_1/linear_alg1.py
@@ -7,7 +7,19 @@ from scipy.integrate import quad
 # MAT 350 - Linear Algebra
 # Homework 1
 
-# Question 1 
+# Question 1
+'''
+x^1 + 2x^2 = 8
+x^1 - x^2 = 2
+
+[1 2 | 8]
+[1 -1 | 2]
+
+x1 = 4
+x2 = 2
+'''
+
+# Question 2
 '''
 x^2 + 3x^3 = 3
 x^1 + 2x^2 + 2x^3 = -5
@@ -38,5 +50,27 @@ This problem has no solution because the 3rd row is the inequality:
 0 = 20
 '''
 
+# Question 3
+'''
+[1 h 4]
+[5 10 16]
+
+Subtract 5 * row 1 from row 2:
+[1 h 4]
+[0 10 - 5h | -4]
+
+Rewrite as a system of linear equations:
+x^1 + hx^2 = 4
+(10 - 5h)x^2 = -4
+
+If h = 2, the equation is undefined, so h != 2
+'''
+
+
+# Question 4
+# Multiple Choice
+
+# Question 5
+# Multiple Choice
 
 
