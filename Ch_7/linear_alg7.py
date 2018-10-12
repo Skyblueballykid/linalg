ff6ed2d10d10cdc55983df45ad3f7d72080c4655
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-10-12 15:16:00 -0500

    Add files via upload

--- Ch_7/linear_alg7.py
+++ Ch_7/linear_alg7.py
@@ -6,3 +6,14 @@ from numpy.linalg import solve
 from numpy.linalg import eig
 from scipy.integrate import quad
 
+# Question 1
+'''
+
+		[1][-8]
+basis = [1][ 1]
+		[0][ 2]
+
+The dimension is 2.
+'''
+
+#Question 2
